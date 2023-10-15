06b0ba99bef7953aae0b7bedd6ad0c4603bdc86f
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-10-15 18:52:40 +0200

    feat: add new editions and buy article

--- contracts/src/PayPer.sol
+++ contracts/src/PayPer.sol
@@ -22,6 +22,7 @@ contract PayPer is Ownable {
         uint256 rating;
         uint256 price;
         uint256 totalPaymentReceived;
+        uint256 date;
         NewsType newsType;
     }
 
@@ -37,42 +38,69 @@ contract PayPer is Ownable {
         uint256 date;
         uint256[] articlesOfEdition;
     }
+
     mapping(uint256 => Article) public articles;
     mapping(address => Journalist) public journalists;
     mapping(uint256 => Edition) public editions;
+    mapping(address => mapping(uint256 => bool)) purchases; //boolean record of if an address has purchased article with uint256 id
 
     uint256 public currentArticleId = 0;
     uint256 public currentEditionId = 0;
 
     constructor(address owner) Ownable(owner) {}
- 
-    function postArticle(string memory name, string memory freeContent, string memory url, uint256 price, uint256 newsType) external {
+
+    function postArticle(
+        string memory name,
+        string memory freeContent,
+        string memory url,
+        uint256 price,
+        uint256 newsType
+    ) external {
         currentArticleId += 1;
         Article memory article = Article({
             id: currentArticleId,
-        journalist: msg.sender,
-        name: name,
-        freeContent: freeContent,
-        encryptedUrl: url,
-        rating: 0,
-        price: price,
-        totalPaymentReceived: 0,
-        newsType: NewsType(newsType)
+            journalist: msg.sender,
+            name: name,
+            freeContent: freeContent,
+            encryptedUrl: url,
+            rating: 0,
+            price: price,
+            totalPaymentReceived: 0,
+            date: block.timestamp,
+            newsType: NewsType(newsType)
         });
 
         articles[currentArticleId] = article;
     }
 
-    function createJournalist(string memory name, string memory description, address journalistAddress) external onlyOwner{
-        
-        Journalist memory journalist = Journalist({
-        id: journalistAddress,
-        name: name,
-        description: description,
-        rating: 0
-        });
+    function createJournalist(string memory name, string memory description, address journalistAddress)
+        external
+        onlyOwner
+    {
+        Journalist memory journalist =
+            Journalist({id: journalistAddress, name: name, description: description, rating: 0});
 
         journalists[journalistAddress] = journalist;
     }
 
+    function createEdition(uint256[] memory articlesOfEdition) external onlyOwner {
+        currentEditionId += 1;
+
+        Edition memory edition =
+            Edition({id: currentEditionId, date: block.timestamp, articlesOfEdition: articlesOfEdition});
+
+        editions[currentEditionId] = edition;
+    }
+
+    function buyArticle(uint256 articleId) external payable {
+        require(articleId <= currentArticleId, "article does not exist");
+        Article memory article = articles[articleId];
+        uint256 price = article.price;
+        require(msg.value >= price, "did not send enough");
+
+        address payable journalist = payable(article.journalist);
+        purchases[msg.sender][articleId] = true;
+
+        journalist.transfer(msg.value);
+    }
 }
