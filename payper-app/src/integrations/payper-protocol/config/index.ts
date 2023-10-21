1f6fafb811560f11a835acf4c23f361fa77abe09
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-10-21 19:21:54 +0200

    feat: read articles from a journalist

--- payper-app/src/integrations/payper-protocol/config/index.ts
+++ payper-app/src/integrations/payper-protocol/config/index.ts
@@ -1,5 +1,6 @@
 import { Address } from "../../../types";
 
 const payperAddress: Address = '0x186265192e01FFED6F844c947d4a528f6781b9ae';
+const subgraphUrl = "https://api.thegraph.com/subgraphs/name/efesozen7/payper-test-1";
 
-export { payperAddress }
+export { payperAddress, subgraphUrl }
--- payper-app/src/integrations/payper-protocol/hooks/read/index.ts
+++ payper-app/src/integrations/payper-protocol/hooks/read/index.ts
@@ -1,9 +1,18 @@
+import { subgraphUrl } from "../../config";
 import useGetAllArticles from "./use-get-all-articles";
 import useGetArticle from "./use-get-article";
 import useGetCurrentArticleId from "./use-get-current-article-id";
 import useGetJournalist from "./use-get-journalist";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: subgraphUrl,
+  cache: new InMemoryCache(),
+});
+
 
 export {
+  client,
   useGetArticle,
   useGetJournalist,
   useGetCurrentArticleId,
